a66ac0676103696022d32209e7f9ea740dee2243
Kirill Kouzoubov <kirillkouzoubov@192-168-1-5.tpgi.com.au>
2012-02-09 20:21:44 +1100

    Adding pretty print, using or instead of nesting

--- src/gccparse/core.clj
+++ src/gccparse/core.clj
@@ -5,7 +5,7 @@
 
 (defn match_infile[l]
   (if-let [m (re-matches #"^In file included from [^:]+:\d+(?::\d+)[,:]$" l)]
-    l))
+    (list l)))
 
 (defn match_hdr[l]
   (if-let [m (re-matches #"^([^:]+): (In .* ‘.*’|At .*):$" l)]
@@ -29,10 +29,22 @@
 
 (defn parse[build_log]
   (defn match[i l]
-    (if-let     [m (match_we     l)] (list :msg    i m)
-      (if-let   [m (match_hdr    l)] (list :in     i m)
-        (if-let [m (match_infile l)] (list :infile i m)))))
+    (or (if-let [m (match_we     l)] (list :msg    i m))
+        (if-let [m (match_hdr    l)] (list :in     i m))
+        (if-let [m (match_infile l)] (list :infile i m))))
   (keep-indexed match build_log))
+
+
+(defn pretty-print[mm]
+  (defn print-record[t l rec]
+    (let [fmt
+          {:infile (fn [l]                  (format "%s"       l        ))
+           :in     (fn [fname msg]          (format "%s:%s"    fname msg))
+           :msg    (fn [fname r c t msg st] (format "%s:%d:%d" fname r c))
+           }]
+      (println (format "%03d %s {%s}"
+                       l t (apply (fmt t) rec)))))
+  (doseq [m mm] (apply print-record m)))
 
 (defn -main [& args]
   (println "Welcome to my project! These are your args:" args))
