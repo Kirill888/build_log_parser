747f7954ee2a88f21e2b5539aa06b2a65cc69047
Kirill Kouzoubov <kirillkouzoubov@192-168-1-5.tpgi.com.au>
2012-02-09 21:29:58 +1100

    replaced applies with de-structuring let syntax.

--- src/gccparse/core.clj
+++ src/gccparse/core.clj
@@ -8,24 +8,22 @@
     (list l)))
 
 (defn match_hdr[l]
-  (if-let [m (re-matches #"^([^:]+): (In .* ‘.*’|At .*):$" l)]
-    (apply (fn [_,fname, msg](list fname, msg)) m)))
+  (if-let [[_ fname msg] (re-matches #"^([^:]+): (In .* ‘.*’|At .*):$" l)]
+    (list fname, msg)))
 
 (defn match_we[l]
   (defn parse-msg [msg]
-    (if-let [m (re-matches #"^(.*) \[([^\[\]]+)\]$" msg)]
-      (list (m 1) (m 2))
+    (if-let [[_ body flags] (re-matches #"^(.*) \[([^\[\]]+)\]$" msg)]
+      (list body flags)
       (list msg "")))
-  
-  (if-let [m (re-matches #"^([^:]+):(\d+):(?:(\d+):|) (warning|error): (.*)$" l)]
-    (apply (fn [_,fname,r,c,t,msg]
-             (list*
-              fname
-              (Integer/parseInt r)
-              (if c (Integer/parseInt c) 0)
-              ({"warning" :warning "error" :error} t)
-              (parse-msg msg)))
-           m)))
+
+  (if-let [[_,fname,r,c,t,msg]
+           (re-matches #"^([^:]+):(\d+):(?:(\d+):|) (warning|error): (.*)$" l)]
+    (list* fname
+           (Integer/parseInt r)
+           (if c (Integer/parseInt c) 0)
+           ({"warning" :warning "error" :error} t)
+           (parse-msg msg))))
 
 (defn parse[build_log]
   (defn match[i l]
@@ -36,15 +34,12 @@
 
 
 (defn pretty-print[mm]
-  (defn print-record[t l rec]
-    (let [fmt
-          {:infile (fn [l]                  (format "%s"       l        ))
-           :in     (fn [fname msg]          (format "%s:%s"    fname msg))
-           :msg    (fn [fname r c t msg st] (format "%s:%d:%d" fname r c))
-           }]
-      (println (format "%03d %s {%s}"
-                       l t (apply (fmt t) rec)))))
-  (doseq [m mm] (apply print-record m)))
+  (let [fmt {:infile (fn [l] (format "%s" l ))
+             :in     (fn [fname msg] (format "%s:%s" fname msg))
+             :msg    (fn [fname r c t msg st]
+                       (format "%s:%d:%d (%s)" fname r c msg))}]
+    (doseq [[t l rec] mm]
+      (println (format "%03d%7s {%s}" l t (apply (fmt t) rec))))))
 
 (defn -main [& args]
   (println "Welcome to my project! These are your args:" args))
