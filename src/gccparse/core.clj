fe69ccad7bee742690cc4c5a97066a9dde5ac712
Kirill Kouzoubov <Kirill888@gmail.com>
2012-02-14 23:44:45 +1100

    added "instantiated from" parsing.

--- src/gccparse/core.clj
+++ src/gccparse/core.clj
@@ -9,12 +9,19 @@
    "fatal error" :fatal-error})
 
 (defn match-infile[l]
-  (if-let [[_,f,r] (re-matches #"^(?:In file included | +)from ([^:]+):(\d+)(?::\d+|)[,:]$" l)]
+  (if-let [[_ f r] (re-matches #"^(?:In file included | +)from ([^:]+):(\d+)(?::\d+|)[,:]$" l)]
     (list f (Integer/parseInt r))))
 
 (defn match-hdr[l]
   (if-let [[_ fname msg] (re-matches #"^([^:]+): (In .*|At .*):$" l)]
-    (list fname, msg)))
+    (list fname msg)))
+
+(defn match-inst[l]
+  (if-let [[_ fname r c from]
+           (re-matches #"^([^:]+):(\d+):(?:(\d+):|)   instantiated from (.*)$" l)]
+    (list fname
+          (Integer/parseInt r) (if c (Integer/parseInt c) 0)
+          from)))
 
 (defn match-we[l]
   (defn parse-msg [msg]
@@ -22,7 +29,7 @@
       (list body flags)
       (list msg "")))
 
-  (if-let [[_,fname,r,c,t,msg]
+  (if-let [[_ fname r c t msg]
            (re-matches #"^([^:]+):(\d+):(?:(\d+):|) (note|warning|error|fatal error): (.*)$" l)]
     (list* fname
            (Integer/parseInt r) (if c (Integer/parseInt c) 0)
@@ -31,15 +38,17 @@
 
 (defn parse[build_log]
   (defn match[i l]
-    (or (if-let [m (match-we     l)] (list :msg    i m))
-        (if-let [m (match-hdr    l)] (list :in     i m))
-        (if-let [m (match-infile l)] (list :infile i m))))
+    (or (if-let [m (match-we      l)] (list :msg    i m))
+        (if-let [m (match-inst    l)] (list :inst   i m))
+        (if-let [m (match-hdr     l)] (list :in     i m))
+        (if-let [m (match-infile  l)] (list :infile i m))))
   (keep-indexed match build_log))
 
 
 (defn pretty-print[mm]
   (let [fmt {:infile (fn [fname r  ] (format "%s:%d" fname r ))
              :in     (fn [fname msg] (format "%s:%s" fname msg))
+             :inst   (fn [fname r c from] (format "%s:%d:%d (%s)" fname r c from))
              :msg    (fn [fname r c t msg st]
                        (format "%s:%d:%d (%s)" fname r c msg))}]
     (doseq [[t l rec] mm]
